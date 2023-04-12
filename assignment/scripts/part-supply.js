8302320c37e35088375ae513f29b52d8a93b9eec
Adam Oberle <oberle.adam@gmail.com>
2023-04-12 11:49:40 -0500

    finished 5 in assignment 3.3

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -25,6 +25,8 @@ console.log( `The last Supply change was removed which was ${removedItem}` );
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+supplyChanges.push( '25' );
+console.log( supplyChanges );
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -32,7 +34,12 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-
+for (let i = 0; i < supplyChanges.length; i++ ){
+    console.log( 'for loop:', supplyChanges[i] );
+    if( supplyChanges[i] > 0 ){
+        console.log( `Added ${supplyChanges[i]} parts`)
+    }
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
